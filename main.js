6536d3f22d3d0482be8709a57441bed52a32a6a0
김서진 <nafi6@naver.com>
2025-05-11 21:37:48 +0900

    5st

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function Login() {
     // JSON으로 사용자 정보 읽기
     var user = JSON.parse(saveUser)
     if (user.password == password) {
-      localStorage.setItem('loginUser', userID)
+      sessionStorage.setItem('loginUser', userID)
       alert('로그인 성공!')
       window.location.href = 'main.html'
     } else {
